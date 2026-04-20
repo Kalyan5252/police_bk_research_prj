ba84fbf4d43aa29e480d8b359edb471e664d5795
Kalyan5252 <kalyanpendem007@gmail.com>
2026-04-20 14:55:15 +0530

    stg2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.routes import loaders
+from app.routes import loaders
 
 app = FastAPI(title="Data Loaders API", version="1.0.0")
 
